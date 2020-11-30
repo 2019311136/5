4f4ed61821683bc064ee999f4e687d17e9a7cd6c
2019311136 <72390382+2019311136@users.noreply.github.com>
2020-11-30 20:33:42 +0800

    Add files via upload

---
+++ PoetryUtils.java
@@ -0,0 +1,24 @@
+package ÊµÑé5;
+
+public class PoetryUtils {
+	   public String processPoetry(String str){
+	        String poetry = "";
+	        int count = 0;
+	        for (int i = 1; i <= str.length(); i++) {
+	            if (i%7 == 0){
+	                count++;
+	                if (count%2 == 0)
+	                    poetry += str.substring(i-7,i)+"¡£\n";
+	                else
+	                    poetry += str.substring(i-7,i)+"£¬";
+	            }
+	        }
+	        return poetry;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
